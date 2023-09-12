68d154a28f6fcda1b42f8799f3fc368a5c80d872
CLochstampfor60 <cflochjr@gmail.com>
2023-09-11 20:05:33 -0400

    finished exercise 1

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -4,13 +4,37 @@ namespace MethodsExercise
 {
     public class Program
     {
-        static void Main(string[] args)
+        public static int Add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        public static int Subtract(int num1, int num2)
+        {
+            return num1 - num2;
+        }
+
+        public static int Multiply(int num1, int num2)
+        {
+            return (num1 * num2);
+        }
+
+        public static int Divide(int num1, int num2)
+        {
+            return (num1 / num2);
+        }
+
+        public static int Modulus(int num1, int num2)
+        {
+            return num1 % num2;
+        }
+
+        private static void Main(string[] args)
         {
           
             Console.WriteLine("What is your name?");
             string yourName = Console.ReadLine();
               
-
             Console.WriteLine("What is your favorite color?");
             string favColor = Console.ReadLine();
             
@@ -27,6 +51,13 @@ namespace MethodsExercise
             string favBand = Console.ReadLine();
 
             Console.WriteLine($"{yourName} visited {favVacation} earlier this year. The spot had some of the best {favMusic} in the city. In fact, {favBand} was scheduled to play too. Unfortunately, the {favVacation} was terrorized by a giant {favAnimal}. {favBand} was trampled and eaten alive by the giant {favAnimal}. It was a sad day: after the horrendous event, it began to rain {favColor}. It was as if the heavens were mourning over the tragic loss.");
+
+            Console.WriteLine("Choose a number:");
+            string int1 = Console.ReadLine();
+
+            Console.WriteLine("Choose a another number:");
+            string int2 = Console.ReadLine();
+
 
         }
 
