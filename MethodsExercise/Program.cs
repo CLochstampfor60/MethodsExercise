602d0bfc8340795893bfad177fe8f9b7439911b1
CLochstampfor60 <cflochjr@gmail.com>
2023-09-12 13:16:30 -0400

    started ex. 2

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -4,31 +4,7 @@ namespace MethodsExercise
 {
     public class Program
     {
-        public static int Add(int num1, int num2)
-        {
-            return num1 + num2;
-        }
-
-        public static int Subtract(int num1, int num2)
-        {
-            return num1 - num2;
-        }
-
-        public static int Multiply(int num1, int num2)
-        {
-            return (num1 * num2);
-        }
-
-        public static int Divide(int num1, int num2)
-        {
-            return (num1 / num2);
-        }
-
-        public static int Modulus(int num1, int num2)
-        {
-            return num1 % num2;
-        }
-
+        //Exercise 1 STARTS here
         private static void Main(string[] args)
         {
           
@@ -51,15 +27,48 @@ namespace MethodsExercise
             string favBand = Console.ReadLine();
 
             Console.WriteLine($"{yourName} visited {favVacation} earlier this year. The spot had some of the best {favMusic} in the city. In fact, {favBand} was scheduled to play too. Unfortunately, the {favVacation} was terrorized by a giant {favAnimal}. {favBand} was trampled and eaten alive by the giant {favAnimal}. It was a sad day: after the horrendous event, it began to rain {favColor}. It was as if the heavens were mourning over the tragic loss.");
+            //Exercise 1 ENDS here
 
-            Console.WriteLine("Choose a number:");
-            string int1 = Console.ReadLine();
+            // *******
 
-            Console.WriteLine("Choose a another number:");
-            string int2 = Console.ReadLine();
+            // Exercise 2 STARTS here
+            Console.WriteLine("Please provide a number to add");
+            int num1 = int.Parse(Console.ReadLine());
 
+            Console.WriteLine("Please provide a number to add to the prior number");
+            int num2 = int.Parse(Console.ReadLine());
 
+            int sum = Sum(num1, num2);  
+            Console.WriteLine($"The sum is: {sum}");
+
+            Console.WriteLine("Please provide a number to subtract");
+            int x = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Please provide a number to subtract");
+            int y = int.Parse(Console.ReadLine());
+        }
+        public static int Sum (int num1, int num2) { 
+          return num1 + num2; }
+
+        public static int Subtract(int x, int y)
+        {
+            return x - y;
         }
 
+        public static int Multiply(int a, int b)
+        {
+            return (a * b);
+        }
+
+        public static int Divide(int c, int d)
+        {
+            return (c / d);
+        }
+
+        public static int Modulus(int e, int f)
+        {
+            return e % f;
+        }
+        // Exercise 2 ENDS here
     }
 }
